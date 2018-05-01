e79ff889a11954cd6f74c3c7b03c590e49412127
George Angel <github-7574494a@infosecproject.org>
2018-05-01 11:08:23 +0100

    Add random strings to etcd instance names
    Force instances recreation on userdata change

--- etcd.tf
+++ etcd.tf
@@ -1,6 +1,6 @@
 // Volumes
 resource "google_compute_disk" "etcd-data" {
-  count = "${length(var.available_zones)}"
+  count = "${var.etcd_instance_count}"
   name  = "etcd-data-${count.index}-${var.cluster_name}"
   zone  = "${var.available_zones[count.index]}"
   size  = "${var.etcd_data_volume_size}"
@@ -15,17 +15,28 @@ resource "google_compute_disk" "etcd-data" {
 }
 
 resource "null_resource" "etcd_data_volume_ids" {
-  count = "${length(var.available_zones)}"
+  count = "${var.etcd_instance_count}"
 
   triggers {
     volume_id = "etcd_data_${count.index}"
   }
 }
 
+resource "random_string" "r" {
+  count   = "${var.etcd_instance_count}"
+  length  = 4
+  special = false
+  upper   = false
+
+  keepers = {
+    userdata = "${var.etcd_user_data[count.index]}"
+  }
+}
+
 // Instances
 resource "google_compute_instance" "etcd" {
-  count       = "${length(var.available_zones)}"
-  name        = "etcd-${count.index}-${var.cluster_name}"
+  count       = "${var.etcd_instance_count}"
+  name        = "etcd-${count.index}-${var.cluster_name}-${random_string.r.*.result[count.index]}"
   description = "etcd cluster member"
 
   machine_type = "${var.etcd_machine_type}"
@@ -111,7 +122,7 @@ resource "google_compute_firewall" "allow-workerss-to-etcds" {
 
 // dns
 resource "google_dns_record_set" "etcd-by-instance" {
-  count = "${length(var.available_zones)}"
+  count = "${var.etcd_instance_count}"
   name  = "${count.index}.etcd.${var.dns_domain}."
   type  = "A"
   ttl   = 30
