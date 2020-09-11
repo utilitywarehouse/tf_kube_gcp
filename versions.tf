f27720636589e0958223c49ecaa15c40668e82e5
George Angel <github-7574494a@infosecproject.org>
2020-09-11 14:09:59 +0100

    Upgrade module to 0.13 terraform

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
